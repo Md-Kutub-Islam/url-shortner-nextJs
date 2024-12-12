b3eac5af6cddaf4d37f555496bb63188e211fb4f
Md-Kutub-Islam <mdkutubislam09@gmail.com>
2024-12-12 21:10:00 +0530

    db-setup-done

---
+++ src/model/db.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+const urlSchema = new mongoose.Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    shortUrl: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export interface IUrl extends Document {
+  originalUrl: String;
+  shortUrl: String;
+}
+
+const Url: Model<IUrl> =
+  mongoose.models.Url || mongoose.model<IUrl>("Url", urlSchema);
+
+export default Url;
